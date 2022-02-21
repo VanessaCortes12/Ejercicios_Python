dd240c1704b98d76018d7fa98e5b3db264f2efc0
VanessaCortes12 <carly_vane124@hotmail.com>
2022-02-20 23:13:09 -0600

    Subiendo los ejercicios

---
+++ Módulo 0/prueba.py
@@ -0,0 +1,10 @@
+# Definimos dos variables llamadas numero1 y
+# numero2, y asociaremos un número a cada una
+
+numero1=float(2.5)
+numero2=float(2.5) 
+
+print (numero1)
+print (numero2)
+
+print ("El resultado es " + str(numero1 + numero2))
